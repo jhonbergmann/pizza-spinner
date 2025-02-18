94b9972f804a6c9de9bf5765e8222734dd2f4f52
Jhonatan Bergmann <54135650+jhonbergmann@users.noreply.github.com>
2025-02-18 00:29:21 -0300

    app

--- app.tsx
+++ app.tsx
@@ -1,20 +1,22 @@
 import {StatusBar} from 'expo-status-bar'
-import {StyleSheet, Text, View} from 'react-native'
+import {useFonts} from 'expo-font'
+
+import Dashboard from './src/screens/dashboard'
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Poppins_400Regular: require('@expo-google-fonts/poppins/Poppins_400Regular.ttf'),
+    Poppins_500Medium: require('@expo-google-fonts/poppins/Poppins_500Medium.ttf'),
+    Poppins_600SemiBold: require('@expo-google-fonts/poppins/Poppins_600SemiBold.ttf'),
+    Poppins_700Bold: require('@expo-google-fonts/poppins/Poppins_700Bold.ttf'),
+  })
+
+  if (!fontsLoaded) return null
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <>
       <StatusBar style="auto" />
-    </View>
+      <Dashboard />
+    </>
   )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-})
